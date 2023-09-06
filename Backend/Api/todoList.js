0e076f10ebd4643fadc7b5b412ca6cde978cd8c3
Baubs <pathogenic@gmail.com>
2023-09-05 20:36:55 -0500

    hooked todoList get to fauna

--- Backend/Api/todoList.js
+++ Backend/Api/todoList.js
@@ -1,7 +1,7 @@
 const faunaKey = require("../../secrets.js");
-const {Ref} = require('faunadb');
+const { Ref } = require("faunadb");
 const faunadb = require("faunadb");
-const url = require('url');
+const url = require("url");
 
 const q = faunadb.query;
 
@@ -10,41 +10,48 @@ const client = new faunadb.Client({
     domain: "db.fauna.com",
 });
 
-async function todoList(req, res){
-    let title = "temp title"
-    let list = [{item: "first item", complete: true}, {item: "first item", complete: false}]
+async function todoList(req, res) {
+    let title = "temp title";
+    let list = [
+        { item: "first item", complete: true },
+        { item: "first item", complete: false },
+    ];
     let user = "dummyUser";
 
     let myUrl = url.parse(req.url);
-    if(req.method == 'POST'){  
-            try {
-                let id = 0;
-                await client.query(
-                    q.NewId()
-                ).then((x) => id = x)
-                const dbs = await client.query(
-                    q.Create(q.Collection("lists"), {
-                        data: {
-                            title: title,
-                            list: list,
-                            user: user,
-                            id: id
-                        }
-                    })
-                )
-                console.log('list created');
-                res.writeHead(200, {'Content-Type': 'application/json'})
-                res.end();
-            }
-            catch (error){
-                console.log("fauna error");
-                res.writeHead(500);
-                res.end();
-                
-            }
-            
-    } else if(req.method == 'GET'){
-
+    if (req.method == "POST") {
+        try {
+            const dbs = await client.query(
+                q.Create(q.Collection("lists"), {
+                    data: {
+                        title: title,
+                        list: list,
+                        user: user,
+                    },
+                })
+            );
+            console.log("list created");
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end();
+        } catch (error) {
+            console.log("fauna error");
+            res.writeHead(500);
+            res.end();
+        }
+    } else if (req.method == "GET") {
+        let dummyId = "375080984463278161";
+        //get id from request, include data in response
+        try {
+            client
+                .query(q.Get(q.Ref(q.Collection("lists"), dummyId)))
+                .then((ret) => console.group(ret));
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end();
+        } catch (error) {
+            console.log("fauna error");
+            res.writeHead(500);
+            res.end();
+        }
     }
 }
 
